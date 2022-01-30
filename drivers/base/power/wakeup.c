dfed6daa21d84bf090656b12bd52d56d35e67040
shashank1436 <9945shashank@gmail.com>
2022-01-30 05:01:32 +0000

    boeffla_wl_blocker: fix compile error

--- drivers/base/power/wakeup.c
+++ drivers/base/power/wakeup.c
@@ -972,12 +972,11 @@ void pm_print_active_wakeup_sources(void)
 	list_for_each_entry_rcu(ws, &wakeup_sources, entry) {
 		if (ws->active) {
 			pr_debug("active wakeup source: %s\n", ws->name);
-			active = 1;
 
-			pr_info("active wakeup source: %s\n", ws->name);
 #ifdef CONFIG_BOEFFLA_WL_BLOCKER
 			if (!check_for_block(ws))	// AP: check if wakelock is on wakelock blocker list
 #endif
+                        active = 1;
 
 		} else if (!active &&
 			   (!last_activity_ws ||
